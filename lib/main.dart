df65c9f754466a6d660149d6e6b9322f4bbb0656
reeli <reeli.null@gmail.com>
2022-06-22 18:23:25 +0800

    add snackbar notification

--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,15 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _visible = true;
 
   @override
   Widget build(BuildContext context) {
@@ -58,39 +65,54 @@ class HomePage extends StatelessWidget {
               child: Text("Rui Li",
                   style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold))),
           Expanded(
-              flex: 1,
-              child: Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Flex(
-                    direction: Axis.horizontal,
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: OutlinedButton(
-                              // style: ElevatedButton.styleFrom(
-                              //     primary: Color.fromRGBO(255, 0, 0, 1)),
-                              onPressed: () {},
-                              child: const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Skip"),
-                              ),
-                            )),
-                      ),
-                      Expanded(
-                        child: Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10),
-                            child: ElevatedButton(
-                              onPressed: () {},
-                              child: const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Accept"),
-                              ),
-                            )),
-                      )
-                    ],
-                  )))
+            flex: 1,
+            child: Container(
+                padding: const EdgeInsets.all(16),
+                child: Visibility(
+                    maintainSize: false,
+                    visible: _visible,
+                    child: Flex(
+                      direction: Axis.horizontal,
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: OutlinedButton(
+                                // style: ElevatedButton.styleFrom(
+                                //     primary: Color.fromRGBO(255, 0, 0, 1)),
+                                onPressed: () {},
+                                child: const Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text("Skip"),
+                                ),
+                              )),
+                        ),
+                        Expanded(
+                          child: Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 10),
+                              child: ElevatedButton(
+                                onPressed: () {
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(const SnackBar(
+                                    content: Text(
+                                        "You'll be today's stand up facilitator, thank you!"),
+                                  ));
+                                  setState(() {
+                                    _visible = false;
+                                  });
+                                },
+                                child: const Padding(
+                                  padding: EdgeInsets.all(16),
+                                  child: Text("Accept"),
+                                ),
+                              )),
+                        )
+                      ],
+                    ))),
+          )
         ]));
   }
 }
