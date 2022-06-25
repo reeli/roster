eb9a24f6b784803c8b850fd3c4f4c0f9fe392209
reeli <reeli.null@gmail.com>
2022-06-25 16:56:31 +0800

    handle json

--- lib/view/RosterListView.dart
+++ lib/view/RosterListView.dart
@@ -1,3 +1,4 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class RosterListView extends StatefulWidget {
@@ -5,6 +6,30 @@ class RosterListView extends StatefulWidget {
 
   @override
   State<RosterListView> createState() => _RosterListViewState();
+}
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Photo(
+      {required this.albumId,
+      required this.id,
+      required this.title,
+      required this.url,
+      required this.thumbnailUrl});
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+        albumId: json["albumId"] as int,
+        id: json["id"] as int,
+        title: json["title"] as String,
+        url: json["url"] as String,
+        thumbnailUrl: json["thumbnailUrl"] as String);
+  }
 }
 
 class _RosterListViewState extends State<RosterListView> {
@@ -16,6 +41,32 @@ class _RosterListViewState extends State<RosterListView> {
     "tomorrow",
     "2022-06-29"
   ];
+
+  void getHttp() async {
+    print("hello");
+    try {
+      var options = BaseOptions(
+        baseUrl: 'https://jsonplaceholder.typicode.com',
+        // connectTimeout: 5000,
+        // receiveTimeout: 3000,
+      );
+      var dio = Dio(options);
+      var response = await dio.get(
+        "/photos",
+      );
+      print(response.data.toString());
+      print("success");
+    } catch (e) {
+      print("error");
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getHttp();
+  }
 
   @override
   Widget build(BuildContext context) {
